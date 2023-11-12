14ae171fba351e9ddcefba10551f8b7434be8837
RaviduHM99 <munasinghemmrh.19@uom.lk>
2023-11-12 21:24:09 +0530

    Initial Commit - Stage 2
    implementatioin for AXI DMA with BRAM

--- rtl/Fetch_Unit.sv
+++ rtl/Fetch_Unit.sv
@@ -1,33 +1,11 @@
 `timescale 1ns/1ps
 
 module Fetch_Unit #(
-    parameter N = 2, REGN = 512,
-              ADDR = 32'h00000000 //Register Address
+    parameter N = 2
 )(
     input logic CLK, RSTN,
-    input logic [31:0] INSTRDATA,
-    input logic [N-1:0][31:0] MAT_IN,
-    input logic DONE,
-    input logic DOUT_MUX,
-    input logic INS_MUX,
-    input logic MATD_MUX,
 
-    output logic [$clog2(REGN) - 1:0] PC,
-    output logic [N-1:0][31:0] MAT_OUT,
-    output logic [31:0] INSTR,
-    output logic [31:0] RESULT,
-    input logic [31:0] DATAOUT
 );
-    reg [$clog2(REGN)-1:0] PC_REG;
-    
-    assign INSTR = (INS_MUX) ? INSTRDATA : 'dz;
-    assign MAT_OUT = (MATD_MUX) ? MAT_IN : 'dz;
-    assign PC = PC_REG;
-    assign RESULT = (DOUT_MUX) ? DATAOUT : 'dz;
 
-    always_ff @(posedge CLK) begin
-        if (RSTN) PC_REG <= ADDR;
-        else PC_REG <= (DONE) ? PC_REG + 'b1 : PC_REG;
-    end
 
 endmodule
--- tb/Processing_Element_tb.sv
+++ tb/Processing_Element_tb.sv
@@ -4,10 +4,6 @@ module Processing_Element_tb;
     localparam CLK_PERIOD = 10;
 
     logic CLK=0, RSTN=0;
-    logic [N-1:0][31:0] DATAIN;
-    logic MAC_CTRL, RST_MUL, INC_PC, MAT_MUX, WRITE_MAT;
-    logic [$clog2(N)-1:0]PC_Counter;
-    logic [31:0] DATAOUT;
 
     Processing_Element #(.N(N)) dut (.*);
 
@@ -18,34 +14,10 @@ module Processing_Element_tb;
     initial begin
         $dumpfile("dump.vcd"); $dumpvars;
         #(CLK_PERIOD*2)
-        @(posedge CLK); //RESET test
+        @(posedge CLK); 
         #1 RSTN <= 1; RST_MUL <= 1;
-        #(CLK_PERIOD) //FETCH MAT A
-        RSTN <= 0; RST_MUL <= 0;
-        MAT_MUX <= 0;
-        INC_PC <= 0;
-        MAC_CTRL <= 0;
-        WRITE_MAT <= 1;
-        DATAIN <= {32'd1, 32'd2, 32'd3, 32'd4, 32'd5, 32'd6, 32'd7, 32'd8, 32'd9, 32'd10, 32'd11, 32'd12, 32'd13, 32'd14, 32'd15, 32'd16};
-        #(CLK_PERIOD*1) //FETCH MAT B
-        MAT_MUX <= 1; 
-        INC_PC <= 0;
-        MAC_CTRL <= 0;
-        WRITE_MAT <= 1;
-        DATAIN <= {32'd1, 32'd2, 32'd3, 32'd4, 32'd5, 32'd6, 32'd7, 32'd8, 32'd9, 32'd10, 32'd11, 32'd12, 32'd13, 32'd14, 32'd15, 32'd16};
-        #(CLK_PERIOD*1) // Vector Multiplication and Accumulation
-        MAT_MUX <= 0;
-        INC_PC <= 1;
-        MAC_CTRL <= 1;
-        WRITE_MAT <= 0;
-        DATAIN <= 'dx;
-        #(CLK_PERIOD*16) // Ending Vecto Ops
-        MAT_MUX <= 0;
-        INC_PC <= 0;
-        MAC_CTRL <= 0;
-        WRITE_MAT <= 0;
-        DATAIN <= 'dx;
-        #(CLK_PERIOD*3)
+        #(CLK_PERIOD) 
+        RSTN <= 0; 
         $finish();
     end
     
